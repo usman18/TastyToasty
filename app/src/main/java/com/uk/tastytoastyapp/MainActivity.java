cfc3595daf36011605a161d6a3afcaeb3b1f3fef
usman18 <secretdeveloper18@gmail.com>
2019-07-11 15:33:39 +0530

    Completed

--- app/src/main/java/com/uk/tastytoastyapp/MainActivity.java
+++ app/src/main/java/com/uk/tastytoastyapp/MainActivity.java
@@ -11,9 +11,21 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		TastyToasty.instaAll(MainActivity.this, "101", "20","60")
+		/*
+		TastyToasty.makeText(MainActivity.this, "This is a custom toast"
+			,TastyToasty.LONG, R.drawable.ic_action_favourite, R.color.violet, R.color.white, true)
 			.show();
+		*/
+		
+		/*
+		new TastyToasty.Builder(MainActivity.this)
+			.setText("This one is using builder method")
+			.setBackgroundColor(R.color.green)
+			.setIconId(R.drawable.ic_verified_user)
+			.showTail(true)
+			.show();
+		*/
+		
 		
 	}
 }
