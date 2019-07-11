33978bad47e5b051e84a11825a4308f82f2937e7
usman18 <secretdeveloper18@gmail.com>
2019-07-11 18:52:50 +0530

    Custom screenshot added

--- app/src/main/java/com/uk/tastytoastyapp/MainActivity.java
+++ app/src/main/java/com/uk/tastytoastyapp/MainActivity.java
@@ -14,18 +14,26 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 		
 		
-
-		TastyToasty.makeText(MainActivity.this, "This is a custom toast",TastyToasty.LONG, R.drawable.ic_action_favourite, R.color.violet, R.color.white, true).show();
-		
 		/*
+		TastyToasty.makeText(MainActivity.this, "This is a custom toast",TastyToasty.LONG, R.drawable.ic_action_favourite, R.color.violet, R.color.white, true).show();
+		*/
+		
+/*
 		new TastyToasty.Builder(MainActivity.this)
 			.setText("This one is using builder method")
 			.setBackgroundColor(R.color.green)
 			.setIconId(R.drawable.ic_verified_user)
-			.showTail(true)
+			.showTail(true) // Pass false or null or don't call at all if you don't want the "tail" in your toast
 			.show();
-		*/
-		
+*/
+
+/*
+		//Default text color is white and default background color is pinkinsh red
+		new TastyToasty.Builder(MainActivity.this)
+			.setIconId(R.drawable.ic_whatshot)
+			.show();
+*/
+	
 		
 	}
 }
