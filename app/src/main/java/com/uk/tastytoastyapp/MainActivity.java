7c04b39cef52fee4a751d96122ec8a5c28ed17b6
usman18 <uk32971@gmail.com>
2020-06-14 13:25:35 +0530

    Underlying libraries version numbers upgraded

--- app/src/main/java/com/uk/tastytoastyapp/MainActivity.java
+++ app/src/main/java/com/uk/tastytoastyapp/MainActivity.java
@@ -1,10 +1,10 @@
 package com.uk.tastytoastyapp;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
 
 import com.uk.tastytoasty.TastyToasty;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 	
@@ -13,11 +13,11 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+/*
 		
-		/*
 		TastyToasty.makeText(MainActivity.this, "This is a custom toast",TastyToasty.LONG, R.drawable.ic_action_favourite, R.color.violet, R.color.white, true).show();
-		*/
-		
+*/
+
 /*
 		new TastyToasty.Builder(MainActivity.this)
 			.setText("This one is using builder method")
--- tastytoasty/src/main/java/com/uk/tastytoasty/TastyToasty.java
+++ tastytoasty/src/main/java/com/uk/tastytoasty/TastyToasty.java
@@ -1,8 +1,8 @@
 package com.uk.tastytoasty;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.v4.content.ContextCompat;
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
